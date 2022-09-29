1736040e7f7a1113fa0678b98f1f6e11380702ec
Mark Steven Reyes <mstevenreyes@gmail.com>
2022-09-29 17:11:45 +0800

    Update inc.insert_employee.php

--- admin/inc.insert_employee.php
+++ admin/inc.insert_employee.php
@@ -14,9 +14,23 @@
     if(isset($submitBtn)){
         $sql = "INSERT INTO $tablename(emp_type, emp_pword, emp_surname, emp_firstname) VALUES (?, ?, ?, ?)";
         $stmt = mysqli_stmt_init($conn, $sql);
-        if(!mysqli_stmt_prepare($stmt)){
-            die("Error")
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            // echo "ERROR: " . mysqli_error($conn);
+            header('location: a_emp_list.php?error=stmtfailed');
+            exit();
         }
+        mysqli_stmt_bind_param($stmt, "ss", 
+        $reqType, $reqDate, $installDate, $distCenter, 
+        $storeCode, $storeName, $address, $openingDate,
+        $longitude, $latitude, $financeCode, $projectCost, $depositStatus,
+        $banknameSource, $accountnumSource,
+        $banknameDest, $accountnumSource, $refNumber, $depositDate);
+        if(!mysqli_stmt_execute($stmt)){
+            echo "ERROR: " . mysqli_error($conn);
+            header('location: r_newstore.php?error=stmtfailed');
+            exit();
+        }
+        header('location:r_newstore.php?success');
     }
 
   
