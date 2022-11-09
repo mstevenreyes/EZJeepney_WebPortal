2d5ba5e1dcb63bb08bc875f33623a70ae35852bf
geannx <santiago.geanne@gmail.com>
2022-11-09 22:30:37 +0800

    ++mali ito pala hehe
    Co-Authored-By: beansentx <83386466+Jvncnt@users.noreply.github.com>

--- ADMIN/a_vehicleRep_inc.php
+++ ADMIN/a_vehicleRep_inc.php
@@ -38,35 +38,30 @@
     
     if(isset($SubmitBtn)){
 
-        if($DI >= $DF){
-            echO"pasokkk";
+        $varEDate = NULL; $varSDate = NULL; //default value is NULL
+        $sqlCheck_DF = "SELECT tb_maintenance.date_issued, tb_maintenance.date_fixed FROM tb_maintenance, tb_jeepney 
+                        WHERE tb_maintenance.date_issued = '$DI' 
+                        AND tb_jeepney.plate_number = '$plateNum';";
+        $testDF = mysqli_query($conn, $sqlCheck_DF);
+        
+        while ($result = mysqli_fetch_assoc($testDF)){
+            $varSDate = $result['date_issued'];
+            $varEDate = $result['date_fixed'];
+        }
+
+
+        if($DF == NULL && $varSDate != NULL){
+            addRecord($conn, $DI, $plateNum, $deets, $reason, $MaintenanceCost);
+        }
+        else if($varEDate == NULL && $varSDate == $DI){
+            addFixedDate($conn, $DI, $DF, $plateNum);
+        }
+        else if($varEDate == NULL && $varSDate == NULL){
+            insert($conn, $DI, $DF, $plateNum, $deets, $reason, $MaintenanceCost);
         }
         else{
-            $varEDate = NULL; $varSDate = NULL; //default value is NULL
-            $sqlCheck_DF = "SELECT tb_maintenance.date_issued, tb_maintenance.date_fixed FROM tb_maintenance, tb_jeepney 
-                            WHERE tb_maintenance.date_issued = '$DI' 
-                            AND tb_jeepney.plate_number = '$plateNum';";
-            $testDF = mysqli_query($conn, $sqlCheck_DF);
-            
-            while ($result = mysqli_fetch_assoc($testDF)){
-                $varSDate = $result['date_issued'];
-                $varEDate = $result['date_fixed'];
-            }
-
-
-            if($DF == NULL && $varSDate != NULL){
-                //addRecord($conn, $DI, $plateNum, $deets, $reason, $MaintenanceCost);
-            }
-            else if($varEDate == NULL && $varSDate == $DI){
-                //addFixedDate($conn, $DI, $DF, $plateNum);
-            }
-            else if($varEDate == NULL && $varSDate == NULL){
-                //insert($conn, $DI, $DF, $plateNum, $deets, $reason, $MaintenanceCost);
-            }
-            else{
-            }
-            header('location: a_vehicleRep.php');
         }
+        header('location: a_vehicleRep.php');
 
     }
 
