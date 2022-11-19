af788888777241827c9f3cbe71dc094e82a4428e
geannx <santiago.geanne@gmail.com>
2022-11-20 03:32:19 +0800

    ++
    Co-Authored-By: beansentx <83386466+Jvncnt@users.noreply.github.com>

--- ADMIN/salary_report.php
+++ ADMIN/salary_report.php
@@ -45,7 +45,9 @@
             <div class="page-breadcrumb bg-white">
                 <div class="row align-items-center">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Salary Reports - Majetsco</h4>            
+                        </br>
+                            <h2 class="page-title">Salary Reports - Majetsco</h2>  
+                        </br>          
                     </div> 
                 </div>
                 <!-- /.col-lg-12 -->
@@ -64,17 +66,17 @@
                     <div class="col-sm-12">
                         <div class="white-box">
                             <div style="text-align:center">
-                                <h3 class="box-title">PAYSLIP FOR THE WEEK OF [INSERT MONTH HERE]</h3>
+                                <h1 class="box-title">PAYSLIP FOR THE WEEK OF [INSERT MONTH HERE]</h1>
                                 <!-- <button class="btn-add-driver btn open-form" style="margin-left: auto;bottom: 50px;">Add Schedule</button> -->
                             </div>
                             <div class="payslip-details">
-                                <div class="company-details">
-                                    <h3>Majetsco Cooperative</h3>
-                                    <img src="images/majetsco-logo.png" alt="" style="width: 80px;">
-                                    <p>29 Gov. Pascual Ave</p>
-                                    <p>Malabon, 1470 Metro Manila</p>
+                                <div class="company-details" style="text-align:center">
+                                    <h3 class="box-title">Majetsco Cooperative</h3>
+                                    <img  class="box-title" src="images/majetsco-logo.png" alt="" style="width: 100px;">
+                                    <h4 class="box-title" >29 Gov. Pascual Ave</h4>
+                                    <h4 class="box-title" >Malabon, 1470 Metro Manila</h4></br>
                                 </div>
-                                <div class="employee-details">
+                                <div class="employee-details box-title" style="margin-left: 10px">
                                     <?php
                                     require_once '../dbh.inc.php';
                                     //temp variables
@@ -82,7 +84,7 @@
                                     $philhealth = 'P150';
                                     $sss = 'P150';
                                     $salary = $_GET['salary-id'];
-                                    $stmt = "SELECT tbs.salary_id, tbs.emp_id, tbs.basic_salary, tbs.canteen_fees, tbs.other_deductions, tbe.emp_type, tbe.emp_surname, tbe.emp_firstname
+                                    $stmt = "SELECT tbs.salary_id, tbs.emp_id, tbs.days_worked, tbs.basic_salary, tbs.canteen_fees, tbs.oth_allow, tbs.grosspay, tbs.netpay, tbs.other_deductions, tbs.othd_amnt, tbe.emp_type, tbe.emp_surname, tbe.emp_firstname
                                      FROM tb_salary_report AS tbs LEFT JOIN tb_employee AS tbe ON  tbs.emp_id = tbe.emp_id WHERE tbs.salary_id = '$salary'";
                                     $sr = mysqli_query($conn, $stmt);
                                     
@@ -92,30 +94,38 @@
                                         echo "<p>" . $result['emp_firstname'] . " " . $result['emp_surname'] . "</p>";
                                         echo "<p>" . $salary . "</p>" . "<p>" . $result['emp_type'] . "</p>";
                                     ?>
-                            
+                                    </br>
                                 </div>
                                 <div class="salary-details">
                                     <div class="earning-details">
                                         <h3>Earnings</h3>
                                         <div class="earning-details-child">
-                                            <p>Basic Salary</p>
+                                            <p>Days Worked</p>
+                                            <p class="amount"><?php echo $result['days_worked']; ?></p>
+                                        </div>
+                                        <div class="earning-details-child">
+                                            <p>Daily Wage</p>
                                             <p class="amount"><?php echo $result['basic_salary']; ?></p>
                                         </div>
                                         <div class="earning-details-child">
-                                            <p>Canteen Fees</p>
-                                            <p class="amount"><?php echo $result['canteen_fees']; ?></p>
+                                            <p>Gross Pay</p>
+                                            <p class="amount"><?php echo $result['grosspay']; ?></p>
                                         </div>
                                         <div class="earning-details-child">
-                                            <p>Other Allowance</p>
-                                            <p class="amount"><?php echo $result['other_deductions']; ?></p>
+                                            <p>Total Deductions: </p>
+                                            <p class="amount"><?php echo -$result['grosspay']; ?></p>
                                         </div>
-                                        <div class="earning-details-child">
-                                            <p>Total Earnings</p>
-                                            <p class="amount">P5500</p>
+                                        <div class="earning-details-child" style="font-weight: bold;">
+                                            <p>Net Pay</p>
+                                            <p class="amount"><?php echo $result['netpay']; ?></p>
                                         </div>
                                     </div>
                                     <div class="deduction-details">
                                         <h3>Deductions</h3>
+                                        <div class="earning-details-child">
+                                            <p>Canteen Fees</p>
+                                            <p class="amount"><?php echo $result['canteen_fees']; ?></p>
+                                        </div>
                                         <div class="earning-details-child">
                                             <p>Pag-ibig</p>
                                             <p class="amount"><?php echo $pag_ibig; ?></p>
@@ -129,27 +139,61 @@
                                             <p class="amount"><?php echo $sss; ?></p>
                                         </div>
                                         <div class="earning-details-child">
-                                            <p>Total Earnings</p>
-                                            <p class="amount">P5500</p>
+                                            <p>Other Deductions</p>
+                                            <p class="amount"><?php echo $result['other_deductions']; ?></p>
                                         </div>
+                                        <div class="earning-details-child">
+                                            <p>Other Deductions Amount</p>
+                                            <p class="amount"><?php echo $result['othd_amnt']; ?></p>
+                                        </div>
+                                       
                                     </div>
                                 </div>
                                 <?php
                                     }
                                 ?>
                             </div>
-                            <div class="table-responsive">
-                            </div>
-
-                            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                                <button type="submit" name="submit" class="btn-edit btn edit-form open-form">EDIT EMPLOYEE SALARY DETAILS</button>
-                            </div>    
+                            <div class="table-responsive">  
+                                    <button type="submit" name="submit" class="btn-edit btn edit-form open-form">EDIT SALARY DETAILS</button>
+                                    <button type="submit" name="delete" class="btn-edit btn edit-form edit-form">DELETE SALARY REPORT</button> 
+                            </div> 
                         </div>
                     </div>
-                            <!-- ADD NEW PARTS OR SUPPLIES -->
+                            <!-- Edit Salary Details -->
                     <div class="form-popup">
                         <div class="container form-wrapper">
                             <button class="btn close-form">Close</button>
+                            <form action="a_inventory_inc.php" method="POST" novalidate="novalidate">
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <h1 class="form-title">Edit Employee Salary</h1>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                <div class="form-group col-sm-6">
+                                        <label for="name">Edit Basic Salary:</label>
+                                        <input type="text" class="form-control" id="item" name="item" required>
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                        <label for="name">Edit Canteen Fees:</label>
+                                        <input type="text" class="form-control" id="item" name="item" required>
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                        <label for="name">Edit Other Deductions:</label>
+                                        <input type="text" class="form-control" id="qty" name="quantity" required>
+                                    </div>
+                                </div>
+                                <div class="form-check">
+                                    <label></label>
+                                </div>
+                                <!-- <input type="submit" name="submit" class="btn send-form" value="Confirm"> -->
+                            </form>
+                        </div>
+                    </div>
+                     <!-- Delete Salary Details -->
+                     <div class="eform-popup">
+                        <div class="container eform-wrapper">
+                            <button class="btn eclose-form">Close</button>
                             <form action="a_inventory_inc.php" method="POST" novalidate="novalidate">
                                 <div class="row">
                                     <div class="col-md-12 text-center">
@@ -259,24 +303,26 @@
             });
         });
 
-        // $(document).ready(function() {
-        //     $('.edit-form').click(function() {
-        //         $('.eform-popup').show();
-        //     });
-        //     $('.eclose-form').click(function() {
-        //         $('.eform-popup').hide();
+     
+
+        $(document).ready(function() {
+            $('.edit-form').click(function() {
+                $('.eform-popup').show();
+            });
+            $('.eclose-form').click(function() {
+                $('.eform-popup').hide();
     
-        //     });
+            });
 
-        //     $(document).mouseup(function(e) {
-        //         var econtainer = $(".eform-wrapper");
-        //         var eform = $(".eform-popup");
+            $(document).mouseup(function(e) {
+                var econtainer = $(".eform-wrapper");
+                var eform = $(".eform-popup");
 
-        //         if (!econtainer.is(e.target) && econtainer.has(e.target).length === 0) {
-        //             eform.hide();
-        //         }
-        //     });
-        // });
+                if (!econtainer.is(e.target) && econtainer.has(e.target).length === 0) {
+                    eform.hide();
+                }
+            });
+        });
     </script>
 
 </body>
