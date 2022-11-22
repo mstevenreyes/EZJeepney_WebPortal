db8393f7661cc0c1cede1eee01537def190a1cae
Leylyr <cortezjkv@gmail.com>
2022-11-23 01:24:32 +0800

    kopya master branch
    ngayon ko lang nagets mag copy :<

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home - EZ JEEPNEY</title>
+  <link rel="stylesheet" href="css/login_style.css">
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+  <script src="https://kit.fontawesome.com/a398ec554b.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class='welcome-page'>
+  <div class='login_fields'>
+      <div class='login_fields__submit index' >
+        <div style="text-align: center;">
+          <img src="images/ez-jeepney-logo-text.png" alt="logo" width="250px">
+        </div>
+        <div style="display: flex;text-align:center">
+          <div style="flex:1;">
+            <a href="emp_login.php"><button><i class="fa-solid fa-users fa-sm"></i><br>Employee</button></a>
+          </div>
+          <div  style="flex:1;text-align:center">
+            <a href="admin_login.php"><button><i class="fa-solid fa-user-tie fa-sm"></i><br>Dispatcher</button></a>
+          </div>
+         </div>
+        
+      </div>
+  </div>
+<script src="login.js"></script>
+</body>
+</html>
