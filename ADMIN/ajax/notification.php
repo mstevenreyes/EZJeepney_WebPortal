023453fc77618fe6502305af87290a168dda26e3
Mark Steven Reyes <mstevenreyes@gmail.com>
2022-12-11 15:35:34 +0800

    added functioning notif system

--- ADMIN/ajax/notification.php
+++ ADMIN/ajax/notification.php
@@ -5,7 +5,7 @@ include('../../dbh.inc.php');
 $command = 'late-employees';
 switch($command){
     case 'late-employees':
-        $sql = "SELECT * FROM view_late_employees";
+        $sql = "SELECT * FROM view_late_employees ORDER BY batch_id";
         $query = mysqli_query($conn, $sql);
         $lateEmpArr = array();
         while($result = mysqli_fetch_assoc($query)){
