b75fefad093ba2e2802caba4d5c865fda1f1c007
Mark Steven Reyes <mstevenreyes@gmail.com>
2022-12-12 22:41:32 +0800

    apply leave additional js empside

--- employee/js/pages/dashboard.js
+++ employee/js/pages/dashboard.js
@@ -1,12 +1,14 @@
 $('#apply-leave').click(function(){
    const response = confirm('Are you sure you want to apply a leave for today?');
    if(response){
+        var empId = document.getElementById('emp-id').textContent;
         alert("Leave applied.");
         $.ajax({
             type: "POST",
             url: "ajax/emp_dashboard.php",
-            data: "set=apply-leave"
+            data: "set=apply-leave&emp-id=" + empId
         }).done(function(result) {               
         });
+        console.log();
    }
 });
